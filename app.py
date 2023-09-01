cde70539baa373f3cafb73765d562f1ccfeac46f
Nayoon Lee <nayoon0530@gmail.com>
2023-09-01 10:27:42 +0900

    project init

---
+++ app.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+
+import streamlit as st 
+import numpy as np
+import pandas as pd
+
+def main():
+
+    st.markdown("Hello World")
+    menu = ["Home", "탐색적 자료 분석", "머신러닝", "About"]
+    choice = st.sidebar.selectbox("메뉴", menu)
+
+    if choice == "Home":
+        st.subheader("Home")
+    elif choice == "탐색적 자료 분석":
+        st.subheader("탐색적 자료 분석")
+    elif choice == "머신러닝":
+        st.subheader("머신러닝")
+    else:
+        st.subheader("About")
+
+
+if __name__ == "__main__":
+    main()
